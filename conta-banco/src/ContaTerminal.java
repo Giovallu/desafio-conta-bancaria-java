f150cb3933ed3d93c051f533d6a8fc697e7eb748
Giovallu <giovallu.st@gmail.com>
2025-03-25 17:55:56 -0300

    meu projeto conta banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,20 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.print("Número da Conta: ");
+        int numero = scanner.nextInt();
+
+        System.out.println("Agência: ");
+        String agencia = scanner.next();
+
+        System.out.println("Nome do Cliente: ");
+        String nomecliente = scanner.next();
+
+        System.out.println("Saldo: ");
+        double saldo = scanner.nextDouble();
     }
 }
